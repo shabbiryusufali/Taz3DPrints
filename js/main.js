e39d2a4dbd68d715072e11aae4c35759c9071ed8
shabbiryusufali <shabyusufali@gmail.com>
2021-04-24 17:12:40 -0700

    taz3dprints init

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function toggleNav() {
+    var x = document.getElementById('navMenu');
+    if (window.getComputedStyle(x).display === "block") {
+        document.getElementById('navMenu').style.animation = ' 0.6s ease 0s slideOutNav';
+        document.getElementById('showNav').style.filter = 'invert(100%)';
+        setTimeout(function() {
+            document.getElementById('navMenu').style.display = 'none';
+        }, 450);
+    } else {
+        document.getElementById('navMenu').style.display = 'block';
+        document.getElementById('navMenu').style.animation = ' 0.6s ease 0s slideInNav';
+        document.getElementById('showNav').style.filter = 'invert(0%)';
+
+    }
+}
+
+
+function mailto() {
+
+    var email = "shabz2002786@gmail.com"
+    var mailto_link = 'mailto:' + email + '?subject=' + document.getElementById("inputMsg2").value + '&body=' + document.getElementById("inputMsg1").value + document.getElementById("inputMsg4").value + document.getElementById("inputMsg3").value;
+    window.open(mailto_link, 'emailWindow');
+
+
+}
+
+function destroyClickedElement(event) {
+    document.body.removeChild(event.target);
+}
